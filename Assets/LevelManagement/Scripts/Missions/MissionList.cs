43d89419073f783a8b4db33b1f68e7f82dac994f
unitygameacademy <unitygameacademy@gmail.com>
2020-03-17 10:35:40 -0700

    MissionList ScriptableObjects (lecture 603)

--- Assets/LevelManagement/Scripts/Missions/MissionList.cs
+++ Assets/LevelManagement/Scripts/Missions/MissionList.cs
@@ -7,10 +7,13 @@ namespace LevelManagement.Missions
     [CreateAssetMenu(fileName = "MissionList", menuName = "Missions/Create MissionList", order = 1)]
     public class MissionList : ScriptableObject
     {
-        [SerializeField]
-        private List<MissionSpecs> _missions;
+        #region INSPECTOR
+        [SerializeField] private List<MissionSpecs> _missions;
+        #endregion
 
+        #region PROPERTIES
         public int TotalMissions => _missions.Count;
+        #endregion
 
         public MissionSpecs GetMission(int index)
         {
